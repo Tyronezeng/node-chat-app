28dd911d667007e88bcba0535d0c6831a0b327e3
Tyrone Zeng <zeng.tyrone@gmail.com>
2017-12-02 22:25:54 -0500

    Emitting newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Meng',
-    text: 'Yeah, sure',
-  });
 });
 
 socket.on('newMessage', function (message) {
